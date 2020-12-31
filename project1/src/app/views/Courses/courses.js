d558109a9ab903237631e8a546e2f2365510cab7
lord483 <bhardwajmd1@outlook.com>
2020-12-31 21:47:52 +0530

    Add heading component to each View's JS file

---
+++ project1/src/app/views/Courses/courses.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <React.Fragment>
+      <h1>Courses page</h1>
+    </React.Fragment>
+  );
+}
+
+export default Courses;
---
+++ project1/src/app/views/Gallary/gallary.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Gallary() {
+  return (
+    <React.Fragment>
+      <h1>Gallary page</h1>
+    </React.Fragment>
+  );
+}
+
+export default Gallary;
